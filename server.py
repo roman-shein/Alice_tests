612ecbcc76b93901781d44e0c93b0420ebe7c26c
admin <roman.shein.07@mail.ru>
2025-05-13 22:10:28 +0300

    thirteenth commit

--- server.py
+++ server.py
@@ -36,8 +36,12 @@ def handle_dialog(req, res):
         return
 
     for el in agree_words:
-        if el in req["original_utterance"].lower():
-            agree_user(res)
+        if el in req["request"]["original_utterance"].lower():
+            if not sessionStorage[user_id]["rabbit"]:
+                agree_user(res)
+                buy_rabbit(user_id)
+            else:
+                end_sess(res)
             return
 
     res["response"]["text"] = f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
@@ -51,13 +55,17 @@ def get_suggest(user_id):
     sessionStorage[user_id]["suggest"] = session
 
     if len(suggests) < 2:
-        suggests.append({"title": "Ладно", "url": "https://market.yandex.ru/search?text=слон", "hide": True})
+        if not sessionStorage[user_id]["rabbit"]:
+            suggests.append({"title": "Ладно", "url": "https://market.yandex.ru/search?text=слон", "hide": True})
+        else:
+            suggests.append({"title": "Ладно", "url": "https://market.yandex.ru/search?text=кролик", "hide": True})
     return suggests
 
 
 def new_user(user_id, res):
     sessionStorage[user_id] = {
-        "suggest": ["Не буду.", "Не хочу.", "Отстань!"]
+        "suggest": ["Не буду.", "Не хочу.", "Отстань!"],
+        "rabbit": False
     }
 
     res["response"]["text"] = "Привет! Купи слона!"
@@ -66,6 +74,15 @@ def new_user(user_id, res):
 
 def agree_user(res):
     res["response"]["text"] = "Слона можно найти на Яндекс.Маркете!"
+
+
+def buy_rabbit(user_id):
+    sessionStorage[user_id]["suggest"] = ["Не буду.", "Не хочу.", "Отстань!"]
+    sessionStorage[user_id]["rabbit"] = True
+
+
+def end_sess(res):
+    res["response"]["text"] = "Кролика можно найти на Яндекс.Маркете!"
     res["response"]["end"] = True
 
 
